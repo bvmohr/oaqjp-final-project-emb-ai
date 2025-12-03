758ab5d53d0e681d6d5ed8742637b57bea693d9d
bvmohr <bvmohr2@gmail.com>
2025-12-02 23:26:01 -0500

    completed task 3

---
+++ emotion_detection.py
@@ -0,0 +1,45 @@
+"""
+
+"""
+
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    # Define the URL for the emotion detection API
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Create the payload with the text to be analyzed
+    input = { "raw_document": { "text": text_to_analyse } }
+
+    # Set the headers with the required model ID for the API
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Make a POST request to the API with the payload and headers
+    response = requests.post(url, json=input, headers=header)
+
+    # Parse the response from the API
+    formatted_response = json.loads(response.text)
+
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    dominant_emotion = 'anger'
+    dominant_score = anger_score
+    for emotion, value in formatted_response['emotionPredictions'][0]['emotion'].items():
+        if value > dominant_score:
+            dominant_score = value
+            dominant_emotion = emotion
+
+    result = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+    return result
